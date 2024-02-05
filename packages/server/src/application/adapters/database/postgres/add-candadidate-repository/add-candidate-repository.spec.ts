814551158ad62d538eb62861d7f79a231a2fb465
Paula Nelo <29684161+paulanelo@users.noreply.github.com>
2024-02-04 23:04:32 -0300

    test: ensure candidate repository add the candidate

---
+++ packages/server/src/application/adapters/database/postgres/add-candadidate-repository/add-candidate-repository.spec.ts
@@ -0,0 +1,25 @@
+import client from '../postgres-helper'
+import { AddCandidatePostgresRepository } from './add-candidate-repository'
+
+describe('Add Candidate Repository', () => {
+  beforeAll(async () => {
+    await client.connect()
+  })
+
+  beforeEach(async () => {
+    await client.query('DELETE * FROM candidates')
+  })
+
+  afterAll(async () => {
+    await client.end()
+  })
+
+  test('should return a candidate', async () => {
+    const addCandidateRepository = new AddCandidatePostgresRepository()
+    const candidate = await addCandidateRepository.add({
+      name: 'Caio'
+    })
+    expect(candidate.id).toBeTruthy()
+    expect(candidate.name).toBeTruthy()
+  })
+})

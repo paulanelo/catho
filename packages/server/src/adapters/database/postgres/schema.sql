eff849993609201503b26c99d6e497160d8da748
Paula Nelo <29684161+paulanelo@users.noreply.github.com>
2024-02-05 12:00:52 -0300

    refactor: change schema script

--- packages/server/src/adapters/database/postgres/schema.sql
+++ packages/server/src/adapters/database/postgres/schema.sql
@@ -1,11 +1,34 @@
-create schema catho;
+-- Database: catho
+DROP DATABASE IF EXISTS catho;
 
-CREATE TABLE candidates (id SERIAL PRIMARY KEY, _name VARCHAR );
+CREATE DATABASE catho;
+\c catho;
+DROP TABLE IF EXISTS public.candidates;
 
-create table catho.skills
-(
-  id         SERIAL PRIMARY KEY,
-  name       text,
-  CONSTRAINT fk_candidate FOREIGN KEY(candidate_id) REFERENCES candidate(id)
+CREATE TABLE IF NOT EXISTS public.candidates (
+  id serial PRIMARY KEY,
+  name text NOT NULL
 );
 
+DROP TABLE IF EXISTS public.skills;
+
+CREATE TABLE IF NOT EXISTS public.skills (
+  id serial PRIMARY KEY,
+  name text NOT NULL
+);
+
+DROP TABLE IF EXISTS public.candidate_skill;
+
+CREATE TABLE IF NOT EXISTS public.candidate_skill (
+  id serial PRIMARY KEY,
+  fk_candidate integer NOT NULL,
+  fk_skill integer NOT NULL,
+  CONSTRAINT candidate_id FOREIGN KEY (fk_candidate)
+    REFERENCES public.candidates (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION,
+  CONSTRAINT skill_id FOREIGN KEY (fk_skill)
+    REFERENCES public.skills (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+);

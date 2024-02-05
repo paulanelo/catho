dbe484955f233491c276d576db0dd35f72135274
Paula Nelo <29684161+paulanelo@users.noreply.github.com>
2024-02-04 23:04:13 -0300

    feat: ensure candidate repository add the candidate

---
+++ packages/server/src/application/adapters/database/postgres/add-candadidate-repository/add-candidate-repository.ts
@@ -0,0 +1,15 @@
+import { type Candidate } from '../../../../../domain/entities'
+import { type AddCandidateRepository } from '../../../../../domain/ports/outbound/database/repositories'
+import { type AddCandidateModel } from '../../../../../domain/use-cases/add-candidate'
+import client from '../postgres-helper'
+
+export class AddCandidatePostgresRepository implements AddCandidateRepository {
+  async add (candidate: AddCandidateModel): Promise<Candidate> {
+    const result = await client.query('INSERT INTO candidates (name) VALUES ($1) RETURNING *', [candidate.name])
+    const insertCandidate = result.rows[0]
+    return {
+      id: insertCandidate.id,
+      name: insertCandidate.name
+    }
+  }
+}

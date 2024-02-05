c3874888de1f89ea01ee87a2e305b44bae0f7a73
Paula Nelo <29684161+paulanelo@users.noreply.github.com>
2024-02-04 23:11:34 -0300

    refactor: add dotenv

--- packages/server/src/application/adapters/database/postgres/postgres-helper.ts
+++ packages/server/src/application/adapters/database/postgres/postgres-helper.ts
@@ -1,7 +1,8 @@
+import 'dotenv/config'
 import { Client } from 'pg'
 
 const client = new Client({
-  user: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USERNAME,
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD
